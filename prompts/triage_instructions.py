cd834c95917f57e1e9d5716a903da8e70e62a2c1
Elena <rahimova.elena@gmail.com>
2025-07-08 21:07:07 +0200

    feat: add triage rules classification

---
+++ prompts/triage_instructions.py
@@ -0,0 +1,24 @@
+
+
+def get_triage_instructions(name: str = "John"):
+    return {
+        "triage_rules": {
+            "ignore": "Spam, rude, offensive, or other inappropriate messages. Marketing, sales, or other promotional messages. Non-customer messages.",
+            "notify": "Feedback, product feedback, or other non-urgent customer messages.",
+            "respond": "Help with the product, technical support, urgent issues, account problems, or other customer messages.",
+        },
+        "agent_instructions": f" Use these tools to help {name} to provide the best possible support service and answer customer questions efficiently."
+    }
+
+classification_description = """
+The classification of the message is based on the following rules: 
+"ignore" for spam, rude, offensive, or other inappropriate messages. 
+"notify" for important messages that does not require an immediate response such as feedback, product feedback. 
+"respond" that requires a response to help with the product, technical support, urgent issues, account problems, or other customer messages.
+"""
+
+classification = {
+    "rules": [get_triage_instructions().triage_rules.keys()],
+    "description": classification_description
+}
+
